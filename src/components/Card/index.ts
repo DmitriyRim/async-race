54881a745f9c03eb6af41c31d45b58cbc2490c13
Rimfis <dimitri.rim@yandex.ru>
2023-01-24 15:08:10 +0300

    fix: class Card

--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -15,25 +15,47 @@ class Card {
     }
     createCard() {
         const cardBox = document.createElement('div');
+        const cardControls = document.createElement('div');
+        const model = document.createElement('span');
+        const track = document.createElement('div');
 
-        cardBox.classList.add('card');
-        cardBox.innerHTML = `
-        <div class="card__control">
-            <button class="card__btn btn select" data-id = ${this.data.id}>Select</button>
-            <button class="card__btn btn remove">Remove</button>
-            <span class="card__model">${this.data.name}</span>
-        </div>
-        <div class="card__track">
+        model.classList.add('card__model');
+        model.textContent = this.data.name;
+        cardControls.classList.add('card__control');
+        cardControls.append(this.createSelectBtn(), this.createRemoveBtn(), model);
+
+        track.classList.add('card__track');
+        track.innerHTML = `
             <button class="card__btn btn">A</button>
             <button class="card__btn btn">B</button>
             <div class="card__race">
                 ${getSVGCar(this.data.color)}
                 ${flag}
             </div>
-        </div>
         `;
-        const remove = cardBox.querySelector('.remove');
-        remove?.addEventListener('click', async () => {
+        cardBox.classList.add('card');
+        cardBox.append(cardControls, track);
+        return cardBox;
+    }
+    createSelectBtn(){
+        const btn = document.createElement('button');
+
+        btn.classList.add('card__btn', 'btn');
+        btn.dataset.id = `${this.data.id}`;
+        btn.textContent = 'Select';
+        btn.addEventListener('click', (event) => {
+            const id = (event.currentTarget as HTMLButtonElement).dataset.id;
+            Garage.updatedCar(Number(id));
+        })
+
+        return btn;
+    }
+    createRemoveBtn(){
+        const btn = document.createElement('button');
+
+        btn.classList.add('card__btn', 'btn');
+        btn.textContent = 'Remove';
+        btn.addEventListener('click', async () => {
             removeCar(this.data.id, '/garage/').then(() => {
                 Garage.renderCards();
             });
@@ -41,12 +63,8 @@ class Card {
                 Garage.renderCards();
             });
         });
-        const select = cardBox.querySelector('.select');
-        select?.addEventListener('click', (event) => {
-            const id = (event.currentTarget as HTMLButtonElement).dataset.id;
-            Garage.updatedCar(Number(id));
-        })
-        return cardBox;
+
+        return btn;
     }
 }
 
